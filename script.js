ed273b23626e17d3061ca65f2c77854173614870
Yuri Colares <134891222+colaresyuri@users.noreply.github.com>
2023-07-08 19:46:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new typed("span",{
-    strings:["Elizabeth Elmiz.","web Developer.","UX Designer.","Photographer."],
+    strings:["Yuri Colares","web Developer.","UX Designer.","Photographer."],
     typeSpeed: 70,
     backSpeed: 60,
     loop: true
